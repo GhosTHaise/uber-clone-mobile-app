b30b8529969e5b420372dfb82d986e65eb8a4693
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-08-30 22:02:41 +0300

    fix : tailwind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,9 +4,9 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView className="flex-1 items-center justify-center">
+      <View >
+        <Text>Uber clone !</Text>
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 // tailwind.config.js
 
 module.exports = {
-   content: [],
-   content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-  extend: { },
-},
-plugins: [],
-  }
+  content: [],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
