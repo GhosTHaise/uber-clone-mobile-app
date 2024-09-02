43b9bd83d1f76e392979b1e3a0e16fd85b1a577d
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-09-02 21:10:36 +0300

    feat :  add react-native-swipper

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,9 +1,12 @@
-import { Text, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { useRef, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { router } from 'expo-router'
+import Swiper from 'react-native-swiper'
 
 const OnBoarding = () => {
+    const swiperRef = useRef<Swiper>(null);
+    const [activeIndex, setActiveIndex] = useState(0);
     return (
         <SafeAreaView className='flex h-full items-center justify-between bg-white'>
             <TouchableOpacity
@@ -14,6 +17,32 @@ const OnBoarding = () => {
             >
                 <Text className='text-black text-md font-JakartaBold'>Skip</Text>
             </TouchableOpacity>
+            <Swiper
+                ref={swiperRef}
+                loop={false}
+                dot={<View className='w-8 h-1 mx-1 bg-[#E2E8F0] rounded-full' />}
+                activeDot={<View className='w-8 h-1 mx-1 bg-[#0286FF] rounded-full' />}
+                onIndexChanged={(index) =>
+                    setActiveIndex(index)
+                }
+
+            >
+                <View className='w-full h-full'>
+                    <View className='w-full h-full'>
+                        <View className='w-full h-full'>
+                            {activeIndex === 0 ? (
+                                <View className='w-full h-full'>
+
+                                </View>
+                            ) : (
+                                <View className='w-full h-full'>
+
+                                </View>
+                            )}
+                        </View>
+                    </View>
+                </View>
+            </Swiper>
         </SafeAreaView>
     )
 }
