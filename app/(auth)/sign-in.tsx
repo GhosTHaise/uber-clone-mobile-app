1876c60096c31b1eba1b84b6ad34b01563c982ad
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-09-16 19:43:46 +0300

    feat : signIn function

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,63 +1,88 @@
-import { Text, ScrollView, View, Image } from 'react-native'
-import { useState } from 'react'
-import { icons, images } from '@/constants'
-import InputField from '@/components/inputField'
-import CustomButton from '@/components/customButton'
-import { Link } from 'expo-router'
+import { Text, ScrollView, View, Image } from "react-native";
+import { useCallback, useState } from "react";
+import { icons, images } from "@/constants";
+import InputField from "@/components/inputField";
+import CustomButton from "@/components/customButton";
+import { Link, useRouter } from "expo-router";
+import { useSignIn } from "@clerk/clerk-expo";
 
 const SignIn = () => {
-    const [form, setForm] = useState({
-        email: '',
-        password: '',
-    })
+  const { signIn, setActive, isLoaded } = useSignIn();
+  const router = useRouter();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-    const onSignInPress = () => {
-        console.log('Sign Up Pressed')
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) {
+      return;
     }
 
-    return (
-        <ScrollView className='flex-1 bg-white'>
-            <View className='flex-1 bg-white'>
-                <View className='relative w-full h-[250px]'>
-                    <Image
-                        source={images.signUpCar}
-                        className='z--0 w-full h-[250px]'
-                    />
-                    <Text className='text-2xl text-black font-jakartaSemibold absolute bottom-5 left-5'>
-                        Welcome 👋
-                    </Text>
-                </View>
-                <View className='p-5'>
-                    <InputField
-                        label='Email'
-                        placeholder='Enter your email'
-                        icon={icons.email}
-                        value={form.email}
-                        onChangeText={(value: string) => setForm({ ...form, email: value })}
-                    />
-                    <InputField
-                        label='Password'
-                        placeholder='Enter your password'
-                        icon={icons.lock}
-                        secureTextEntry={true}
-                        value=''
-                        onChangeText={(value: string) => setForm({ ...form, password: value })}
-                    />
-                    <CustomButton
-                        title='Sign Up'
-                        onPress={onSignInPress}
-                        className='mt-6'
-                    />
-                    {/* OAuth */}
-                    <Link href="/sign-up" className='text-lg text-center text-general-200 mt-8'>
-                        <Text>Don't have an account ?</Text>
-                        <Text className='text-primary-500'> Sign Up</Text>
-                    </Link>
-                </View>
-                {/* Verification Modal */}
-            </View>
-        </ScrollView>
-    )
-}
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
 
-export default SignIn
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/");
+      } else {
+        // See https://clerk.com/docs/custom-flows/error-handling
+        // for more info on error handling
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [isLoaded, form.email, form.password]);
+
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z--0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-jakartaSemibold absolute bottom-5 left-5">
+            Welcome 👋
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value: string) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            value=""
+            onChangeText={(value: string) =>
+              setForm({ ...form, password: value })
+            }
+          />
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignInPress}
+            className="mt-6"
+          />
+          {/* OAuth */}
+          <Link
+            href="/sign-up"
+            className="text-lg text-center text-general-200 mt-8"
+          >
+            <Text>Don't have an account ?</Text>
+            <Text className="text-primary-500"> Sign Up</Text>
+          </Link>
+        </View>
+        {/* Verification Modal */}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default SignIn;
