c3729ebab809b4d17f5f239d6f869ae4cc388687
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-09-22 21:44:58 +0300

    feat : show static map from geoapify

--- components/rideCard.tsx
+++ components/rideCard.tsx
@@ -1,10 +1,38 @@
 import { Ride } from "@/types/type";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
-const RideCard = ({ ride }: { ride: Ride }) => {
+const RideCard = ({
+  ride: {
+    destination_longitude,
+    destination_latitude,
+    origin_address,
+    destination_address,
+    created_at,
+    ride_time,
+    driver,
+    payment_status,
+  },
+}: {
+  ride: Ride;
+}) => {
   return (
-    <View>
-      <Text className="text-3xl">{ride.driver.first_name}</Text>
+    <View
+      className="flex flex-row items-center 
+        justify-center bg-white rounded-lg shadow-sm 
+        shadow-neutral-300 mb-3
+        "
+    >
+      <View className="flex flex-row items-center justify-between p-3">
+        <View className="flex flex-row items-center justify-between">
+          <Image
+            source={{
+              uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${destination_longitude},${destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+            }}
+            className="w-[80px] h-[90px] rounded-lg"
+          />
+        </View>
+      </View>
+      <Text className="text-3xl">{driver.first_name}</Text>
     </View>
   );
 };
