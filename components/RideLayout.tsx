c2f2be83f10a49201e3ba9fe45a05432fbcabf9d
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-10-02 21:02:23 +0300

    feat : add @gorhom/bottom-sheet

--- components/RideLayout.tsx
+++ components/RideLayout.tsx
@@ -1,9 +1,10 @@
 import { icons } from "@/constants";
 import { router } from "expo-router";
-import React from "react";
+import React, { useRef } from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Map from "./map";
+import BottomSheet, { BottomSheetScrollView } from "@gorhom/bottom-sheet";
 
 const RideLayout = ({
   title,
@@ -12,6 +13,7 @@ const RideLayout = ({
   title: string;
   children: React.ReactNode;
 }) => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
   return (
     <GestureHandlerRootView>
       <View className="flex-1 bg-white">
@@ -35,6 +37,11 @@ const RideLayout = ({
           </View>
           <Map />
         </View>
+        <BottomSheet ref={bottomSheetRef} snapPoints={["40$", "85%"]} index={0}>
+          <BottomSheetScrollView style={{ flex: 1, padding: 20 }}>
+            {children}
+          </BottomSheetScrollView>
+        </BottomSheet>
       </View>
     </GestureHandlerRootView>
   );
