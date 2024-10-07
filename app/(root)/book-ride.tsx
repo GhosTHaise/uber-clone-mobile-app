eb72c4edab8fa7a224076f148483d42f8c99c33f
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-10-07 20:38:55 +0300

    feat: init payment sheet

--- app/(root)/book-ride.tsx
+++ app/(root)/book-ride.tsx
@@ -20,20 +20,9 @@ const BookRide = () => {
     (driver) => +driver.id === +selectedDriver!,
   )[0];
 
-  const [publishableKey, setPublishableKey] = useState("");
-
-  const fetchPublishableKey = async () => {
-    /* const key = await fetchKey(); // fetch key from your server here
-        setPublishableKey(key); */
-  };
-
-  useEffect(() => {
-    fetchPublishableKey();
-  }, []);
-
   return (
     <StripeProvider
-      publishableKey={publishableKey}
+      publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!}
       merchantIdentifier="merchant.identifier" // required for Apple Pay
       urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
     >
