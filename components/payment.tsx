d77258c6aa99bb9edfdc9aab1bcb76493658891b
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-10-13 19:44:00 +0300

    setup booking confirmation and add success modal

--- components/payment.tsx
+++ components/payment.tsx
@@ -1,11 +1,14 @@
-import { View, Text, Alert } from "react-native";
-import React, { useEffect, useState } from "react";
+import { Alert, Image, Text, View } from "react-native";
+import React, { useState } from "react";
 import CustomButton from "./customButton";
-import { PaymentSheetError, useStripe } from "@stripe/stripe-react-native";
+import { useStripe } from "@stripe/stripe-react-native";
 import { fetchAPI } from "@/lib/fetch";
 import { PaymentProps } from "@/types/type";
 import { useLocationStore } from "@/store";
 import { useAuth } from "@clerk/clerk-expo";
+import ReactNativeModal from "react-native-modal";
+import { images } from "@/constants";
+import { useRouter } from "expo-router";
 
 const Payment = ({
   fullName,
@@ -16,6 +19,7 @@ const Payment = ({
 }: PaymentProps) => {
   const { userId } = useAuth();
   const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const router = useRouter();
   const [isSuccess, setIsSuccess] = useState(false);
   const {
     userLatitude,
@@ -34,103 +38,123 @@ const Payment = ({
           amount: parseInt(amount) * 100,
           currencyCode: "USD",
         },
-        confirmHandler: confirmHandler,
+        confirmHandler: async (paymentMethod, _, intentCreationCallback) => {
+          // Make a request to your own server.
+          const { paymentIntent, customer } = await fetchAPI(
+            "/api/stripe/create",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                name: fullName || email.split("@")[0],
+                email,
+                amount,
+                paymentMethodId: paymentMethod.id,
+              }),
+            },
+          );
+
+          if (paymentIntent.client_secret) {
+            const { result } = await fetchAPI("/api/stripe/pay", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                payment_method_id: paymentMethod.id,
+                payment_intent_id: paymentIntent.id,
+                customer_id: customer,
+              }),
+            });
+
+            if (result.client_secret) {
+              await fetchAPI("/(api)/ride/create", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  origin_address: userAddress,
+                  destination_address: destinationAddress,
+                  origin_latitude: userLatitude,
+                  origin_longitude: userLongitude,
+                  destination_latitude: destinationLatitude,
+                  destination_longitude: destinationLongitude,
+                  ride_time: rideTime.toFixed(0),
+                  fare_price: parseInt(amount) * 100,
+                  payment_status: "paid",
+                  driver_id: driverId,
+                  user_id: userId,
+                }),
+              });
+            }
+
+            // Call the `intentCreationCallback` with your server response's client secret or error
+            const { client_secret, error } = result.client_secret;
+
+            if (client_secret) {
+              intentCreationCallback({ clientSecret: client_secret });
+            } else {
+              intentCreationCallback({ error });
+            }
+          }
+
+          const openPaymentSheet = async () => {
+            await initializePaymentSheet();
+            const { error } = await presentPaymentSheet();
+
+            if (error) {
+              Alert.alert(
+                `Error code : ${error.code}`,
+                `Error message : ${error.message}`,
+              );
+            } else {
+              // Payment completed - show a confirmation screen.
+              setIsSuccess(true);
+            }
+          };
+
+          return (
+            <>
+              <CustomButton
+                title="Confirm Ride"
+                className="my-10"
+                onPress={openPaymentSheet}
+              />
+              <ReactNativeModal
+                isVisible={isSuccess}
+                onBackdropPress={() => setIsSuccess(false)}
+              >
+                <View className="flex flex-col items-center justify-center bg-wjite roudned-2xl">
+                  <Image source={images.check} className="w-28 h-28 mt-5" />
+                  <Text className="text-2xl text-center font-JakartaBold mt-5">
+                    Ride booked!
+                  </Text>
+                  <Text className="text-md text-general-200 font-JakartaMedium text-center mt-3">
+                    Thank you for your booking. Your reservation has been
+                    placed. Please procees with your trip!
+                  </Text>
+                  <CustomButton
+                    title="Back Home"
+                    onPress={() => {
+                      setIsSuccess(false);
+                      router.push("/(root)/(tabs)/home");
+                    }}
+                    className="mt-5"
+                  />
+                </View>
+              </ReactNativeModal>
+            </>
+          );
+        },
       },
       returnURL: 'myapp"//book-ride',
     });
     if (error) {
       console.log({ error });
     }
-  };
-
-  const confirmHandler = async (
-    paymentMethod: any,
-    _: any,
-    intentCreationCallback: any,
-  ) => {
-    // Make a request to your own server.
-    const { paymentIntent, customer } = await fetchAPI("/api/stripe/create", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: fullName || email.split("@")[0],
-        email,
-        amount,
-        paymentMethodId: paymentMethod.id,
-      }),
-    });
-
-    if (paymentIntent.client_secret) {
-      const { result } = await fetchAPI("/api/stripe/pay", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          payment_method_id: paymentMethod.id,
-          payment_intent_id: paymentIntent.id,
-          customer_id: customer,
-        }),
-      });
-
-      if (result.client_secret) {
-        await fetchAPI("/(api)/ride/create", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            origin_address: userAddress,
-            destination_address: destinationAddress,
-            origin_latitude: userLatitude,
-            origin_longitude: userLongitude,
-            destination_latitude: destinationLatitude,
-            destination_longitude: destinationLongitude,
-            ride_time: rideTime.toFixed(0),
-            fare_price: parseInt(amount) * 100,
-            payment_status: "paid",
-            driver_id: driverId,
-            user_id: userId,
-          }),
-        });
-      }
-
-      // Call the `intentCreationCallback` with your server response's client secret or error
-      const { client_secret, error } = result.client_secret;
-
-      if (client_secret) {
-        intentCreationCallback({ clientSecret: client_secret });
-      } else {
-        intentCreationCallback({ error });
-      }
-    }
-
-    const openPaymentSheet = async () => {
-      await initializePaymentSheet();
-      const { error } = await presentPaymentSheet();
-
-      if (error) {
-        Alert.alert(
-          `Error code : ${error.code}`,
-          `Error message : ${error.message}`,
-        );
-      } else {
-        // Payment completed - show a confirmation screen.
-        setIsSuccess(true);
-      }
-    };
-
-    return (
-      <>
-        <CustomButton
-          title="Confirm Ride"
-          className="my-10"
-          onPress={openPaymentSheet}
-        />
-      </>
-    );
   };
 };
 export default Payment;
